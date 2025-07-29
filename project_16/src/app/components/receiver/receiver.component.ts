9be0b21bda630812637d2c10dc9e51f9d833131a
karthikeya <nithya.041591@gmail.com>
2025-07-29 12:58:40 +0530

    eventemitter communication between cchild and parent

--- project_16/src/app/components/receiver/receiver.component.ts
+++ project_16/src/app/components/receiver/receiver.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input ,Output} from '@angular/core';
 
 @Component({
   selector: 'app-receiver',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class ReceiverComponent {
 @Input() rip:any
+@Output() reply = new EventEmitter()
+
+replyMessage(){
+  this.reply.emit("this is childs reply")
+}
 }
--- project_16/src/app/components/sender/sender.component.ts
+++ project_16/src/app/components/sender/sender.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class SenderComponent {
 message="hey buddy from parent"
+incomingMessage=''
 }
